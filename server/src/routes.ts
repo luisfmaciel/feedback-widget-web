509c57668ab2519668c9c0d57695a3688beec34d
Luis <luis.barbosa@al.infnet.eu.br>
2022-05-10 19:44:44 -0300

    Add try catch - routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
-import { NodemailerMailAdapter } from './adapters/nodemailer/NodemailerMailAdapter';
 import { PrismaFeedbacksRepository } from './repositories/prisma/PrismaFeedbacksRepository';
+import { NodemailerMailAdapter } from './adapters/nodemailer/NodemailerMailAdapter';
 import { SubmitFeedback } from './services/SubmitFeedback';
 
 export const routes = Router();

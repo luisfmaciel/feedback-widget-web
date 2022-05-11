2eaba47160786ccee24d9b6941eb56091c2a94e4
Luis <luis.barbosa@al.infnet.eu.br>
2022-05-11 01:59:42 -0300

    Adjust cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,14 +5,8 @@ import cors from "cors";
 const app = express();
 const port = 3333;
 
-app.use(express.json());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  next();
-});
 app.use(cors());
+app.use(express.json());
 app.use(routes);
 
 app.listen(process.env.PORT || port, () => {

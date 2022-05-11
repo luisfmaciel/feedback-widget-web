d35a71f1cf590b88ddcac51221527492a7021c7a
Luis <luis.barbosa@al.infnet.eu.br>
2022-05-11 01:48:56 -0300

    add setHeader

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,8 @@ const port = 3333;
 app.use(express.json());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Headers', '*');
   app.use(cors());
   next();
 });

4037e60cc748263f4245b03139bf6708866d7d7d
Luis <luis.barbosa@al.infnet.eu.br>
2022-05-10 21:45:04 -0300

    add middleware cors

---
+++ server/src/middlewares/cors.ts
@@ -0,0 +1,10 @@
+export const cors = (
+  request: any, response: {
+     setHeader: (key: string, value: string) => void; 
+    }, next: () => void) => {
+  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  response.setHeader('Access-Control-Allow-Methods', '*');
+  response.setHeader('Access-Control-Allow-Headers', '*');
+  response.setHeader('Access-Control-Max-Age', '10');
+  next();
+};
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,12 @@
 import express from 'express';
-import cors from "cors";
+// import cors from "cors";
+import { cors } from './middlewares/cors';
 import { routes } from './routes';
 
 const app = express();
 const port = 3333;
 
-app.use(cors({
-  origin: "*"
-}));
+app.use(cors);
 app.use(express.json());
 app.use(routes)
 

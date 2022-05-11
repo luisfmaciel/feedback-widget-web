637300722fa360cfba6b7f699ce5fdd46a8984c3
Luis <luis.barbosa@al.infnet.eu.br>
2022-05-10 21:18:22 -0300

    Add method POST at cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,9 @@ import { routes } from './routes';
 const app = express();
 const port = 3333;
 
-app.use(cors());
+app.use(cors({
+  "methods": "POST"
+}));
 app.use(express.json());
 app.use(routes)
 

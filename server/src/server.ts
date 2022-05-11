7e60ac36ef4e7db6ef2f647e4c6f24fc6840fd20
Luis <luis.barbosa@al.infnet.eu.br>
2022-05-11 14:30:50 -0300

    Add middleware cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,21 +1,21 @@
 import express from 'express';
 import { routes } from './routes';
-// import cors from "cors";
+import cors from "cors";
 
 const app = express();
-const port = 3333;
+// const port = 3333;
 
 app.use(express.json());
-// app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', '*');
-  res.setHeader('Access-Control-Allow-Headers', '*');
-  res.setHeader('Access-Control-Max-Age', '10');
-  next();
-});
+app.use(cors());
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', '*');
+//   res.setHeader('Access-Control-Allow-Headers', '*');
+//   res.setHeader('Access-Control-Max-Age', '10');
+//   next();
+// });
 app.use(routes);
 
-app.listen(process.env.PORT || port, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log(`HTTP Server running`);
 });

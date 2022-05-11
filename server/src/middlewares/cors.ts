62920c7cba8b8d027f71ecc72a450182df2c7b12
Luis <luis.barbosa@al.infnet.eu.br>
2022-05-10 22:08:26 -0300

    adjust origin

--- server/src/middlewares/cors.ts
+++ server/src/middlewares/cors.ts
@@ -2,7 +2,7 @@ export const cors = (
   request: any, response: {
      setHeader: (key: string, value: string) => void; 
     }, next: () => void) => {
-  response.setHeader('Access-Control-Allow-Origin', 'https://feedback-widget-web-production-dev-luis.up.railway.app/');
+  response.setHeader('Access-Control-Allow-Origin', 'https://feedback-widget-web-ten.vercel.app');
   response.setHeader('Access-Control-Allow-Methods', '*');
   response.setHeader('Access-Control-Allow-Headers', '*');
   response.setHeader('Access-Control-Max-Age', '10');

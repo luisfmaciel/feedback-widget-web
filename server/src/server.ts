537505ca05a3c7c046952b8637e2aa9b3144100b
Luis <luis.barbosa@al.infnet.eu.br>
2022-05-11 14:22:20 -0300

    Adjust cors on server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,19 @@
 import express from 'express';
 import { routes } from './routes';
-import cors from "cors";
+// import cors from "cors";
 
 const app = express();
 const port = 3333;
 
-app.use(cors());
 app.use(express.json());
+// app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Max-Age', '10');
+  next();
+});
 app.use(routes);
 
 app.listen(process.env.PORT || port, () => {
